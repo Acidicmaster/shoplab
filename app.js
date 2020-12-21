264ba602ff23a95ec9d12f53c0b65db3e9f199b8
Kenneth Uchechukwu <acidicmaster@outlook.com>
2020-12-21 12:41:07 +0100

    After Db connection

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 var createError = require('http-errors');
 var express = require('express');
-//var path = require('path');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var connectdb = require('./config/db');
+const dotenv = require("dotenv");
 
-//var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
+//configure env
+dotenv.config()
+//connecting to database
+connectdb();
 
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
